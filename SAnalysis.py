271485b9f4c4f65274c74bc037b84176b9e14dc0
MikeyOfCantley <70953033+MikeyOfCantley@users.noreply.github.com>
2021-02-24 12:28:17 +0000

    Update SAnalysis.py

--- SAnalysis.py
+++ SAnalysis.py
@@ -1,10 +1,17 @@
-# import matlab 
-import matlab
 import json
-from pprint import pprint
 
+total = 0
+#open dictionary
 json_data=open('dictionary.json')
 jdata = json.load(json_data)
-
+#open report and split into list of words
 report = open("report.txt").read()
-print = report
+words = report.split()
+#check the list of words against the dictionary and return value
+for element in words:
+  if element in jdata:
+    value = jdata[element]
+    total += value
+
+print(total)
+
